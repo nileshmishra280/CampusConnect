6ce33653bb258724ecb004608d4c2bab37c7ee41
aliabbas222004 <aliabbasbhaisaheb22@gmail.com>
2025-06-24 09:56:23 +0530

    launch

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-  host:true,
-  allowedHosts: ['campusconnect-71g3.onrender.com'],
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: ['campusconnect-71g3.onrender.com'],
+  },
+});
